5d6a0c0fa346e4c236d68fac79d179d1f11c8652
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-12-19 13:43:44 +0900

    graphqlを実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  root 'blogs#index'
+  resources :blogs
+
   if Rails.env.development?
       mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
     end
   post "/graphql", to: "graphql#execute"
-  root 'blogs#index'
-  resources :blogs
 end
